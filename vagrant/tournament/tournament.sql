fd2de686f5e95389bb7e341eb26520ab1b0c398f
Oscar <oscarchang1226@gmail.com>
2016-12-30 14:18:35 -0600

    adding tournament schemas

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,29 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE players (
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE matches (
+    id      SERIAL PRIMARY KEY,
+    player1 INTEGER NOT NULL REFERENCES players,
+    player2 INTEGER NOT NULL REFERENCES players,
+    mRound  INTEGER NOT NULL
+);
+
+CREATE TABLE results (
+    matchId INTEGER NOT NULL REFERENCES matches,
+    winner  INTEGER NOT NULL REFERENCES players,
+    loser   INTEGER NOT NULL REFERENCES players
+);
+
+CREATE VIEW standings AS (
+    SELECT
+        players.id, players.name, r.wins, m.plays
+    FROM
+        players, (
+            SELECT
+        )
+);
